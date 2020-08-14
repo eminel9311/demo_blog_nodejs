ac2ed214de75013a8729d79592b90791251e1775
quanbh <quanbh@lika.edu.vn>
2020-08-14 20:30:57 +0700

    init commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const ejs = require('ejs');
 //regist public folder
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 app.listen(3000, () => {
   console.log('server start at port 3000');
 })
